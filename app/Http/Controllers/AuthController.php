b22dc1027f00c83cc74b4829504ddd81b8321559
Jussi Alanen <jussi.alanen@hiondigital.com>
2026-03-14 14:42:33 +0200

    Update the resume and endpoints, Docker setup optimizations for composer

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -95,10 +95,7 @@ class AuthController extends Controller
 
         $token = $user->createToken('api')->plainTextToken;
 
-        return response()->json([
-            'token' => $token,
-            'user' => $user,
-        ]);
+        return response()->json(['token' => $token]);
     }
 
     /**
