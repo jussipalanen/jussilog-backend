faf19e770d20c4bc4354bd62dfe086f258624d83
Jussi Alanen <juzapala@gmail.com>
2026-03-27 13:06:57 +0200

    Projects table

---
+++ app/Enums/ProjectVisibility.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+/**
+ * Enum representing the visibility state of a project.
+ */
+enum ProjectVisibility: string
+{
+    /** Project is publicly visible. */
+    case SHOW = 'show';
+
+    /** Project is hidden from public view. */
+    case HIDE = 'hide';
+
+    /**
+     * Get the human-readable label for the visibility value.
+     *
+     * @return string
+     */
+    public function label(): string
+    {
+        return match ($this) {
+            self::SHOW => 'Show',
+            self::HIDE => 'Hide',
+        };
+    }
+}
